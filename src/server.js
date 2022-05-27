1f06af79f6e1652b2f71dded77bdfec6d11e00cd
erica7monteiro@gmail.com <erikaisa7dev@gmail.com>
2022-05-26 23:38:42 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use('/', require('./route/postsRoute'))
+
+app.use((error, req, res, next) => {
+  if (error.message === 'Post already exists') {
+    return res.status(409).send(e.message)
+  }
+  if (error.message === 'Post not found') {
+    return res.status(404).send(e.message)
+  }
+  return res.status(500).send(e.message)
+})
+
+app.listen(3000)
